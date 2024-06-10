7a1f1709b499547d81586d5d7c1df825700f798e
Aditya  Matukumalli <adityamatukumalli@Adityas-MacBook-Pro.local>
2024-06-10 16:56:01 -0400

    fix landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,33 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  if (!user) {
+    return redirect("/login");
+  } else {
+    return redirect("/dashboard");
+  }
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+  // return (
+  //   <div className="flex-1 w-full flex flex-col gap-20 items-center">
+  //     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+  //       <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+  //         <AuthButton />
+  //       </div>
+  //     </nav>
 
-      <div>
-        <Header />
-      </div>
+  //     <div>
+  //       <Header />
+  //     </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs"></footer>
-    </div>
-  );
+  //     <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs"></footer>
+  //   </div>
+  // );
 }
